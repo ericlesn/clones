378d5b5adb715b463caa716d3bc99b7d580dde42
ericlesn <ericlesnunes1@outlook.com>
2021-04-14 19:00:40 -0300

    Criação das barras de servidores e canais

--- clone-ui-reactJS/clone-discord/src/components/ChannelButton/styles.ts
+++ clone-ui-reactJS/clone-discord/src/components/ChannelButton/styles.ts
@@ -1,4 +1,6 @@
 import styled from 'styled-components';
+import { Hashtag } from 'styled-icons/heroicons-outline';
+import { PersonAdd, Settings } from 'styled-icons/material'
 
 export const Container = styled.div`
     display: flex;
@@ -32,12 +34,38 @@ export const Container = styled.div`
     }
   
 `;
-export const HashtagIcon = styled.div`
+export const HashtagIcon = styled(Hashtag)`
+    width:20px;
+    height:20px;
+    
+    color: var(--symbol);
   
 `; 
-export const InviteIcon = styled.div`
-  
+export const InviteIcon = styled(PersonAdd)`
+    width:16px;
+    height:16px;
+    
+    color: var(--symbol);
+    cursor: pointer;
+
+    transition: color 0.2s;
+
+    &:hover{
+        color: var(--white)
+    }
 `;
-export const SettingsIcon = styled.div`
-  
+export const SettingsIcon = styled(Settings)`
+    width:16px;
+    height:16px;
+
+    margin-left:4px;
+    
+    color: var(--symbol);
+    cursor: pointer;
+
+    transition: color 0.2s;
+
+    &:hover{
+        color: var(--white)
+    }
 `;
