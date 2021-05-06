3dccada4b426a0b29d306f3bac93a4569ca3264a
ericlesn <ericlesnunes1@outlook.com>
2021-05-05 21:33:46 -0300

    criação do layout estático

---
+++ clone-ui-reactJS/clone-pipefy-drag-n-drop/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Board from './components/Board';
+
+import Header from './components/Header';
+
+
+import GlobalStyles from './styles/GlobalStyles'
+
+function App() {
+  return(
+    <>
+      <Header/>
+      <Board/>
+      <GlobalStyles/>
+
+    </>
+  )
+}
+
+export default App;
---
+++ clone-ui-reactJS/clone-pipefy-drag-n-drop/src/components/Header/styles.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  height: 80px;
+  padding: 0 30px;
+  background: var(--rocketseat-purple);
+  color: var(--white);
+
+  display:flex;
+  align-items: center;
+
+`;
